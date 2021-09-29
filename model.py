0bfdac238504e99b94e21602c9670b53998a838c
kangsanha <68986305+sanha9999@users.noreply.github.com>
2021-09-29 23:55:03 +0900

    alexnet -> ResNet

--- model.py
+++ model.py
@@ -2,47 +2,41 @@ import torch
 import torch.nn as nn
 import numpy as np
 from torch.nn.modules.activation import ReLU
+from torch.nn.modules.pooling import MaxPool2d
 
 class TitaModel(nn.Module):
     def __init__(self):
         super(TitaModel, self).__init__()
 
-        # model (alexnet)
-        self.titamodel = nn.Sequential(
-            nn.Conv2d(in_channels=3, out_channels=96, kernel_size=11, padding=0, stride=4), # 227 -> 55
-            nn.ReLU(inplace=True),
-            nn.LocalResponseNorm(size=5, k=2),
-            nn.MaxPool2d(kernel_size=3, stride=2), 
-            
-            nn.Conv2d(in_channels=96, out_channels=256, kernel_size=5, padding=2, stride=1), # 27 -> 27
-            nn.ReLU(inplace=True),
-            nn.LocalResponseNorm(size=5, k=2),
-            nn.MaxPool2d(kernel_size=3, stride=2), 
-            
-            nn.Conv2d(in_channels=256, out_channels=384, kernel_size=3, padding=1, stride=1),
-            nn.ReLU(inplace=True),
-            nn.LocalResponseNorm(size=5, k=2),
-            nn.Conv2d(in_channels=384, out_channels=384, kernel_size=3, padding=1, stride=1),
-            nn.ReLU(inplace=True),
-            nn.LocalResponseNorm(size=5, k=2),
-            nn.Conv2d(in_channels=384, out_channels=256, kernel_size=3, padding=1, stride=1),
-            nn.ReLU(inplace=True),
-            nn.LocalResponseNorm(size=5, k=2),
-            nn.MaxPool2d(kernel_size=3, stride=2), 
-        )
-
-        self.fclayer = nn.Sequential(
-            nn.Linear(256 * 6 * 6, 4096),
-            nn.ReLU(inplace=True),
-            nn.Dropout(p=0.5),
-            nn.Linear(4096, 4096),
-            nn.ReLU(inplace=True),
-            nn.Dropout(p=0.5),
-            nn.Linear(4096, 2),
-        )
+        # model (ResNet)
+        
 
     def forward(self, x):
-        x = self.convnet(x)
-        x = torch.flatten(x, 1)
-        x = self.fclayer(x)
+
         return x
+
+def conv_1(): # model start
+    return nn.Sequential(
+        nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=4),
+        nn.BatchNorm2d(64),
+        nn.ReLU(inplace=True),
+        nn.MaxPool2d(kernel_size=3, stride=2)
+    )
+
+def bottleneck_block(in_channels, mid_channels, out_channels, down=False):
+    layers = []
+    if down:
+        layers.append(nn.Conv2d(in_channels, mid_channels, kernel_size=1, stride=2, padding=0))
+    else:
+        layers.append(nn.Conv2d(in_channels, mid_channels, kernel_size=1, stride=1, padding=0))
+    layers.extend([
+        nn.BatchNorm2d(mid_channels),
+        nn.ReLU(inplace=True),
+        nn.Conv2d(mid_channels, mid_channels, kernel_size=3, stride=1, padding=1),
+        nn.BatchNorm2d(mid_channels),
+        nn.ReLU(inplace=True),
+        nn.Conv2d(mid_channels, out_channels, kernel_size=1, stride=1, padding=0),
+        nn.BatchNorm2d(out_channels),
+    ])
+    return nn.Sequential(*layers)
+
